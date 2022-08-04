f5f1ab084d29e54a6b25314ecbe4be35d8ddf24d
Joallyson-Pereira <joallysonpere@gmail.com>
2022-08-04 19:44:09 -0300

    top

---
+++ script.js
@@ -0,0 +1,38 @@
+class ValidaFormulario {
+    constructor() {
+        this.formulario = document.querySelector('.formulario');
+
+        this.eventos();
+    }
+
+    eventos() {
+        this.formulario.addEventListener('submit', e => {
+            this.handleSubmit(e);
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const camposValidos = this.camposSaoValidos();
+    }
+
+    camposSaoValidos() {
+        let valid = true;
+
+        for (let campo of this.formulario.querySelectorAll('.input')) {
+            if(!campo.value) {
+                this.criaErro(campo, 'Esse campo não pode estar em branco')
+            }
+    
+        }
+    }
+
+    criaErro(campo, msg) {
+        const div = document.createElement('div');
+        div.innerHTML = msg;
+        div.classList.add('error-text');
+        campo.insertAdjacentElement('afterend', div);
+    }
+
+}    
+const valida = new ValidaFormulario();
